8737d353db8de4578167f0f45d6c80aeda13fd88
ABlued <53801395+ABlued@users.noreply.github.com>
2021-12-12 15:36:15 +0900

    on/off 버튼 기능 구현하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,14 +12,25 @@ function App() {
           <button
             data-testid="minus-button"
             onClick={() => setCounter(counter - 1)}
+            disabled={disabled}
           >
             -
           </button>
           <button
             data-testid="plus-button"
             onClick={() => setCounter(counter + 1)}
+            disabled={disabled}
           >
             +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: 'blue' }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
           </button>
         </div>
       </header>
--- src/App.test.js
+++ src/App.test.js
@@ -36,3 +36,17 @@ test('When the - button is pressed, the counter changes to -1', () => {
   const counterElement = screen.getByTestId('counter');
   expect(counterElement).toHaveTextContent(-1);
 });
+// 이 테스트만 스킵한다.
+test.skip('on/off button has blue color', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+});
+// 이 테스트만 실행한다.
+test.only('Prevent the -,+ button from being pressed when the on/off button is cliecked', () => {
+  render(<App />);
+  const onOffButtonElement = screen.getByTestId('on/off-button');
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  expect(plusButtonElement).toBeDisabled();
+});
