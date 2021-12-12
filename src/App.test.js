4cdc2c7409c87f2b5e82331cd0a6da506f6f20bc
ABlued <53801395+ABlued@users.noreply.github.com>
2021-12-12 15:16:06 +0900

    카운터와 button 뷰 구현하기

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.queryByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해서 원하는 element에 접근(접근할 때 ID로)
+  const counterElement = screen.getByTestId('counter');
+  // id가 counter인 element의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId('minus-button');
+  expect(minusButtonElement).toHaveTextContent('-');
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  expect(plusButtonElement).toHaveTextContent('+');
 });
